59be393ce91de4bc9931b2c17ba9e1e88f0a81c5
Ali Yazdizadeh <aliyzd1379@gmail.com>
2022-08-11 17:38:08 +0200

    add zero tree tests

--- tests/test_midpoint.py
+++ tests/test_midpoint.py
@@ -1,4 +1,21 @@
-from utils.midpoint import midpoint_rooting_longest_path, midpoint_rooting_outgroup, find_outlier_leaves
+from utils.midpoint import midpoint_rooting_outgroup, find_outlier_leaves
+from ete3 import PhyloTree
+
+def test_zero_length_tree_1():
+    gene_tree = PhyloTree('../test_data/zero_tree_1.tree')
+    outliers = find_outlier_leaves(gene_tree)
+    outgroup = midpoint_rooting_outgroup(gene_tree)
+    assert outgroup == gene_tree.get_midpoint_outgroup()
+    outgroup = midpoint_rooting_outgroup(gene_tree, outliers)
+
+
+def test_zero_length_tree_2():
+    gene_tree = PhyloTree('../test_data/zero_tree_2.tree')
+    outliers = find_outlier_leaves(gene_tree)
+    outgroup = midpoint_rooting_outgroup(gene_tree)
+    assert outgroup == gene_tree.get_midpoint_outgroup()
+    outgroup = midpoint_rooting_outgroup(gene_tree, outliers)
+
 
 def test_longest_path():
     assert 1 == 1
