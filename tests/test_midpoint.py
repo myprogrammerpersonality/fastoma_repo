fcd12c292ce6c0aba45382187b5e47ce25d4c4ea
Ali Yazdizadeh <aliyzd1379@gmail.com>
2022-08-11 13:56:28 +0200

    add basic test

---
+++ tests/test_midpoint.py
@@ -0,0 +1,12 @@
+from utils.midpoint import midpoint_rooting_longest_path, midpoint_rooting_outgroup, find_outlier_leaves
+
+def test_longest_path():
+    assert 1 == 1
+
+
+def test_outgroup():
+    assert 1 == 1
+
+
+def test_outlier():
+    assert 1 == 1
